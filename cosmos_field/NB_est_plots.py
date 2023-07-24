6dc0b6db2910a91859b781b18904eff679f02af6
ShaneBechtel <shane.bechtel@yahoo.com>
2023-07-24 15:11:59 -0700

    Modified code to show comoving figure as well for sanity check.

--- cosmos_field/NB_est_plots.py
+++ cosmos_field/NB_est_plots.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.cosmology import Planck18
 from astropy import units as u
-
+from IPython import embed
 
 # Get npz files
 
@@ -20,11 +20,11 @@ mean_sep_527 = (1 + 3.3) * Planck18.angular_diameter_distance(3.3) * ang_527.to(
 mean_sep_718 = (1 + 4.9) * Planck18.angular_diameter_distance(4.9) * ang_718.to(u.rad).value
 mean_sep_816 = (1 + 5.7) * Planck18.angular_diameter_distance(5.7) * ang_816.to(u.rad).value
 
-# Make Figure
 
+# Make Figure
 plt.figure(figsize=(15,10))
 
-plt.title("(CURRENTLY DO NOT TRUST NB718 & NB816 SIMS)")
+#plt.title("(CURRENTLY DO NOT TRUST NB718 & NB816 SIMS)")
 
 plt.plot(nb527['M1450'], nb527['DIST'], 'r', label='Distance to Gamma_QSO = Gamma_UVB in NB527 (z=3.3)')
 plt.plot(nb718['M1450'], nb718['DIST'], 'g', label='Distance to Gamma_QSO = Gamma_UVB in NB718 (z=4.9)')
@@ -35,7 +35,27 @@ plt.hlines(mean_sep_718.value,-22,-27, 'g', linestyles='--', label='Mean Sightli
 plt.hlines(mean_sep_816.value,-22,-27, 'b', linestyles='--', label='Mean Sightline Separation in NB816 (z=5.7)')
 
 plt.xlim(-22, -27)
+plt.ylim(0,50)
 plt.xlabel(r'M_{1450}')
 plt.ylabel('Distance (pMpc)')
+plt.legend()
+plt.show()
+
+
+
+plt.figure(figsize=(15,10))
+
+plt.plot(nb527['M1450'], nb527['DIST']/4.3, 'r', label='Distance to Gamma_QSO = Gamma_UVB in NB527 (z=3.3)')
+plt.plot(nb718['M1450'], nb718['DIST']/5.9, 'g', label='Distance to Gamma_QSO = Gamma_UVB in NB718 (z=4.9)')
+plt.plot(nb816['M1450'], nb816['DIST']/6.7, 'b', label='Distance to Gamma_QSO = Gamma_UVB in NB816 (z=5.7)')
+
+plt.hlines(mean_sep_527.value/4.3,-22,-27, 'r', linestyles='--', label='Mean Sightline Separation in NB527 (z=3.3)')
+plt.hlines(mean_sep_718.value/5.9,-22,-27, 'g', linestyles='--', label='Mean Sightline Separation in NB718 (z=4.9)')
+plt.hlines(mean_sep_816.value/6.7,-22,-27, 'b', linestyles='--', label='Mean Sightline Separation in NB816 (z=5.7)')
+
+plt.xlim(-22, -27)
+plt.ylim(0,8)
+plt.xlabel(r'M_{1450}')
+plt.ylabel('Distance (cMpc)')
 plt.legend()
 plt.show()
